7e7c60cbd742ee08c74b6ca220a8d1a51deaaa53
Logan Kojiro <lkojiro@andrew.cmu.edu>
2020-11-16 00:44:28 -0500

    Added skeleton opengl code

--- fluid_sim/CPU/CPU.cpp
+++ fluid_sim/CPU/CPU.cpp
@@ -1,11 +1,37 @@
-// CPU.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include<GL/glut.h>
 
-#include <iostream>
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 0.0, 0.0);
 
-int main()
-{
-    std::cout << "Hello World!\n";
+	glBegin(GL_POINTS);
+	glVertex2f(10.0, 10.0);
+	glVertex2f(150.0, 80.0);
+	glVertex2f(100.0, 20.0);
+	glVertex2f(200.0, 100.0);
+	glEnd();
+	glFlush();
+}
+
+void myinit() {
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glColor3f(1.0, 0.0, 0.0);
+	glPointSize(5.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, 499.0, 0.0, 499.0);
+}
+
+void main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Points");
+	glutDisplayFunc(display);
+
+	myinit();
+	glutMainLoop();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
